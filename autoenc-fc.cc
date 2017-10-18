e71e42b5652ded02014d2690269c0f2019d5a4c6
Hao Tang <haotang@csail.mit.edu>
2017-10-18 11:59:16 -0400

    Tie autoencoder weights

--- autoenc-fc.cc
+++ autoenc-fc.cc
@@ -2,6 +2,42 @@
 
 namespace autoenc {
 
+    std::shared_ptr<tensor_tree::vertex> make_symmetric_ae_tensor_tree()
+    {
+        tensor_tree::vertex root { "nil" };
+    
+        root.children.push_back(tensor_tree::make_tensor("fc weight"));
+        root.children.push_back(tensor_tree::make_tensor("fc bias"));
+        root.children.push_back(tensor_tree::make_tensor("fc bias"));
+
+        return std::make_shared<tensor_tree::vertex>(root);
+    }
+
+    std::shared_ptr<autodiff::op_t> make_symmetric_ae(std::shared_ptr<autodiff::op_t> input,
+        std::shared_ptr<tensor_tree::vertex> var_tree,
+        double input_dropout, double hidden_dropout,
+        std::default_random_engine& gen)
+    {
+        if (input_dropout != 0.0) {
+            auto mask = autodiff::dropout_mask(input, input_dropout, gen);
+            input = autodiff::emul(mask, input);
+        }
+    
+        auto z = autodiff::mul(input, tensor_tree::get_var(var_tree->children[0]));
+        auto b1 = autodiff::rep_row_to(tensor_tree::get_var(var_tree->children[1]), z);
+        auto h = autodiff::relu(autodiff::add(z, b1));
+
+        if (hidden_dropout != 0.0) {
+            auto mask = autodiff::dropout_mask(h, hidden_dropout, gen);
+            h = autodiff::emul(mask, h);
+        }
+    
+        z = autodiff::rtmul(h, tensor_tree::get_var(var_tree->children[0]));
+        auto b2 = autodiff::rep_row_to(tensor_tree::get_var(var_tree->children[2]), z);
+    
+        return autodiff::add(z, b2);
+    }
+
     std::shared_ptr<tensor_tree::vertex> make_tensor_tree(int layer)
     {
         tensor_tree::vertex root { "nil" };
--- autoenc-fc.h
+++ autoenc-fc.h
@@ -5,6 +5,13 @@
 
 namespace autoenc {
 
+    std::shared_ptr<tensor_tree::vertex> make_symmetric_ae_tensor_tree();
+
+    std::shared_ptr<autodiff::op_t> make_symmetric_ae(std::shared_ptr<autodiff::op_t> input,
+        std::shared_ptr<tensor_tree::vertex> var_tree,
+        double input_dropout, double hidden_dropout,
+        std::default_random_engine& gen);
+
     std::shared_ptr<tensor_tree::vertex> make_tensor_tree(int layer);
 
     std::shared_ptr<autodiff::op_t> make_nn(std::shared_ptr<autodiff::op_t> input,
